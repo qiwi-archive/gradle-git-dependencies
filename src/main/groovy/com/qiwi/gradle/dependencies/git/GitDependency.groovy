4417d2bc7bedb539390f194d76d1f7fe9aba15c3
Ilya Linnik <i.linnik@qiwi.ru>
2015-04-16 15:09:09 +0300

    Buildable project

--- src/main/groovy/com/qiwi/gradle/dependencies/git/GitDependency.groovy
+++ src/main/groovy/com/qiwi/gradle/dependencies/git/GitDependency.groovy
@@ -1,3 +1,11 @@
-class GitDependency {
+package com.qiwi.gradle.dependencies.git
 
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GitDependency implements Plugin<Project> {
+
+    void apply(Project target) {
+
+    }
 }
