cf0d5e25e644557adfc7d4d6eeb1ccb28c4b1710
Ilya Linnik <i.linnik@qiwi.ru>
2015-04-16 16:29:23 +0300

    Modified .gitignore file

---
+++ src/main/java/com/qiwi/gradle/dependencies/git/GitArtifactDependency.java
@@ -0,0 +1,19 @@
+package com.qiwi.gradle.dependencies.git;
+
+import org.gradle.api.artifacts.repositories.ArtifactRepository;
+
+/**
+ * Created by nixan on 16.04.15.
+ */
+public class GitArtifactDependency implements ArtifactRepository {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setName(java.lang.String name) {
+
+    }
+}
